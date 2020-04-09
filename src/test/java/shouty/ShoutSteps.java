37c6d80ff8e9d6fa977bab5ac32ab8885b90f04f
Stefan Baychev <stefan.baychev@tide.co>
2020-04-09 11:11:51 +0300

    fixing tests with params

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -13,24 +13,24 @@ public class ShoutSteps {
     private static final String ARBITRARY_MESSAGE = "Hello, world";
     private final Shouty shouty = new Shouty();
 
-    @Given("{name} is at {int}, {int}")
+    @Given("{string} is at {int}, {int}")
     public void lucy_is_at(String name, int xCoord, int yCoord) {
         shouty.setLocation(name, new Coordinate(xCoord, yCoord));
     }
 
-    @Given("{name} is at {int}, {int}")
-    public void sean_is_at(String name, int xCoord, int yCoord) {
-        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
-    }
+//    @Given("{string} is at {int}, {int}")
+//    public void sean_is_at(String name, int xCoord, int yCoord) {
+//        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
+//    }
 
-    @When("{name} shouts")
+    @When("{string} shouts")
     public void sean_shouts(String name) {
         shouty.shout(name, ARBITRARY_MESSAGE);
     }
 
-    @Then("{Lucy} should hear Sean")
-    public void lucy_should_hear_sean() {
-        assertEquals(1, shouty.getShoutsHeardBy("Lucy").size());
+    @Then("{string} should hear Sean")
+    public void lucy_should_hear_sean(String shout_hear) {
+        assertEquals(1, shouty.getShoutsHeardBy(shout_hear).size());
     }
 
     @Then("Lucy should hear nothing")
--- src/test/resources/shouty/hear_shout.feature
+++ src/test/resources/shouty/hear_shout.feature
@@ -3,25 +3,25 @@ Feature: Hear Shout
   Shouts have a range of approximately 1000m
 
   Scenario: In range shout is heard
-    Given Lucy is at 0, 0
-    And Sean is at 0, 900
-    When Sean shouts
-    Then Lucy should hear Sean
+    Given "Lucy" is at 0, 0
+    And "Sean" is at 0, 900
+    When "Sean" shouts
+    Then "Lucy" should hear Sean
 
   Scenario: Out of range shout is not heard
-    Given Lucy is at 0, 0
-    And Sean is at 3000, 3000
-    When Sean shouts
+    Given "Lucy" is at 0, 0
+    And "Sean" is at 3000, 3000
+    When "Sean" shouts
     Then Lucy should hear nothing
 
   Scenario: Slightly Above out of range shout is not heard
-    Given Lucy is at 0, 0
-    And Sean is at 1001, 1001
-    When Sean shouts
+    Given "Lucy" is at 0, 0
+    And "Sean" is at 1001, 1001
+    When "Sean" shouts
     Then Lucy should hear nothing
 
   Scenario: Slightly Below Top Range shout is heard
-    Given Lucy is at 0, 0
-    And Sean is at 0, 999
-    When Sean shouts
-    Then Lucy should hear Sean
+    Given "Lucy" is at 0, 0
+    And "Sean" is at 0, 999
+    When "Sean" shouts
+    Then "Lucy" should hear Sean
