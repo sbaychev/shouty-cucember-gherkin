605024d4903b4e4cceb578b1326b8e85c925c5f6
Stefan Baychev <stefan.baychev@tide.co>
2020-04-09 11:36:43 +0300

    PendingException on missing step definition

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -1,11 +1,13 @@
 package shouty;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 
 public class ShoutSteps {
@@ -13,28 +15,34 @@ public class ShoutSteps {
     private static final String ARBITRARY_MESSAGE = "Hello, world";
     private final Shouty shouty = new Shouty();
 
-    @Given("{string} is at {int}, {int}")
+    @Given("{word} is at {int}, {int}")
     public void lucy_is_at(String name, int xCoord, int yCoord) {
         shouty.setLocation(name, new Coordinate(xCoord, yCoord));
     }
 
-//    @Given("{string} is at {int}, {int}")
-//    public void sean_is_at(String name, int xCoord, int yCoord) {
-//        shouty.setLocation(name, new Coordinate(xCoord, yCoord));
-//    }
-
-    @When("{string} shouts")
+    @When("{word} shouts")
     public void sean_shouts(String name) {
         shouty.shout(name, ARBITRARY_MESSAGE);
     }
 
-    @Then("{string} should hear Sean")
+    @Then("{word} should hear Sean")
     public void lucy_should_hear_sean(String shout_hear) {
         assertEquals(1, shouty.getShoutsHeardBy(shout_hear).size());
     }
 
-    @Then("Lucy should hear nothing")
-    public void lucy_should_hear_nothing() {
-        assertEquals(emptyMap(), shouty.getShoutsHeardBy("Lucy"));
+    @Then("{word} should hear nothing")
+    public void lucy_should_hear_nothing(String shout_consumer) {
+        assertEquals(emptyMap(), shouty.getShoutsHeardBy(shout_consumer));
+    }
+
+    @Then("{word} should not hear {word}")
+    public void lucyShouldNotHearOscar(String shout_consumer,  String shout_producer) {
+        assertFalse(shouty.getShoutsHeardBy(shout_consumer).containsKey(shout_producer));
+    }
+
+    @Then("Lucy should shout at Oscar")
+    public void lucyShouldShoutAtOscar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
--- src/test/resources/shouty/hear_shout.feature
+++ src/test/resources/shouty/hear_shout.feature
@@ -3,25 +3,35 @@ Feature: Hear Shout
   Shouts have a range of approximately 1000m
 
   Scenario: In range shout is heard
-    Given "Lucy" is at 0, 0
-    And "Sean" is at 0, 900
-    When "Sean" shouts
-    Then "Lucy" should hear Sean
+    Given Lucy is at 0, 0
+    And Sean is at 0, 900
+    When Sean shouts
+    Then Lucy should hear Sean
 
   Scenario: Out of range shout is not heard
-    Given "Lucy" is at 0, 0
-    And "Sean" is at 3000, 3000
-    When "Sean" shouts
+    Given Lucy is at 0, 0
+    And Sean is at 3000, 3000
+    When Sean shouts
     Then Lucy should hear nothing
 
   Scenario: Slightly Above out of range shout is not heard
-    Given "Lucy" is at 0, 0
-    And "Sean" is at 1001, 1001
-    When "Sean" shouts
+    Given Lucy is at 0, 0
+    And Sean is at 1001, 1001
+    When Sean shouts
     Then Lucy should hear nothing
 
   Scenario: Slightly Below Top Range shout is heard
-    Given "Lucy" is at 0, 0
-    And "Sean" is at 0, 999
-    When "Sean" shouts
-    Then "Lucy" should hear Sean
+    Given Lucy is at 0, 0
+    And Sean is at 0, 999
+    When Sean shouts
+    Then Lucy should hear Sean
+
+  Scenario: Multiple shouters
+    Given Lucy is at 0, 0
+    And Sean is at 0, 500
+    And Oscar is at 1100, 0
+    When Sean shouts
+    And Oscar shouts
+    Then Lucy should not hear Oscar
+    Then Lucy should shout at Oscar
+    But Lucy should hear Sean
